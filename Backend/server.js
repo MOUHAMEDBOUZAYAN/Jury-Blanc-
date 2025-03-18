0fc2b5ff29c8f9c418c3c141bf350ceb9894c3c9
mouhamedbouzayan <bouzimohammed177@gmail.com>
2025-03-18 13:43:43 +0000

    Update the models

--- Backend/server.js
+++ Backend/server.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api/auth', authRoutes);
+app.use('/a pi/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 
 // Démarrer le serveur
