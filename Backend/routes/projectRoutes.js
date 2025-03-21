e9a2c2623dcb595825379a502adf5577c60c0f9e
mouhamedbouzayan <bouzimohammed177@gmail.com>
2025-03-21 11:26:51 +0000

    Added Fichier TaskRoutes (Craude Opiration

--- Backend/routes/projectRoutes.js
+++ Backend/routes/projectRoutes.js
@@ -1,67 +1,13 @@
 const express = require('express');
-const Project = require('../models/Project');
 const router = express.Router();
+const {
+  getProjects,
+  createProject,
+  updateProject,
+  deleteProject,
+} = require('../controllers/projectController');
 
-// Créer un nouveau projet
-router.post('/', async (req, res) => {
-  const { name, description, startDate, endDate, budget, createdBy } = req.body;
-
-  try {
-    const project = new Project({ name, description, startDate, endDate, budget, createdBy });
-    await project.save();
-    res.status(201).json(project);
-  } catch (error) {
-    res.status(500).json({ message: 'Something went wrong' });
-  }
-});
-
-// Récupérer tous les projets
-router.get('/', async (req, res) => {
-  try {
-    const projects = await Project.find().populate('createdBy', 'username');
-    res.status(200).json(projects);
-  } catch (error) {
-    res.status(500).json({ message: 'Something went wrong' });
-  }
-});
-
-// Récupérer un projet par ID
-router.get('/:id', async (req, res) => {
-  try {
-    const project = await Project.findById(req.params.id).populate('createdBy', 'username');
-    if (!project) {
-      return res.status(404).json({ message: 'Project not found' });
-    }
-    res.status(200).json(project);
-  } catch (error) {
-    res.status(500).json({ message: 'Something went wrong' });
-  }
-});
-
-// Mettre à jour un projet
-router.put('/:id', async (req, res) => {
-  try {
-    const project = await Project.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    if (!project) {
-      return res.status(404).json({ message: 'Project not found' });
-    }
-    res.status(200).json(project);
-  } catch (error) {
-    res.status(500).json({ message: 'Something went wrong' });
-  }
-});
-
-// Supprimer un projet
-router.delete('/:id', async (req, res) => {
-  try {
-    const project = await Project.findByIdAndDelete(req.params.id);
-    if (!project) {
-      return res.status(404).json({ message: 'Project not found' });
-    }
-    res.status(200).json({ message: 'Project deleted successfully' });
-  } catch (error) {
-    res.status(500).json({ message: 'Something went wrong' });
-  }
-});
+router.route('/').get(getProjects).post(createProject);
+router.route('/:id').put(updateProject).delete(deleteProject);
 
 module.exports = router;
